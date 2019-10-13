069f6eb9bf43b5ca918c5b43ade38846926368b7
joseck ogachi <jogachi4@gmail.com>
2019-10-13 15:33:50 +0300

    aded changes to files

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '8k6!ni8t*8b=^x7$$yjxihn2pifao!5h6x%vggxyas)tv7h4i3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['']
 
 
 # Application definition
